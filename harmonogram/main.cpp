d6cb9349a84e17727891ac71638833fdae4a9104
jradziejewski <261309@student.pwr.edu.pl>
2022-03-12 21:52:12 +0100

    rearrange(main): Comment out menu to work on features manually

--- harmonogram/main.cpp
+++ harmonogram/main.cpp
@@ -3,51 +3,52 @@
 
 
 int main() {
-	Time clock;
-	bool menu = true;
-	bool submenu = true;
-	char suboption;
-	int number;
+	Time c1;
+	Time c2;
+	//bool menu = true;
+	//bool submenu = true;
+	//char suboption;
+	//int number;
 
-	while (menu) {
-		char option;
-		std::cout << "1. Ustaw sekundy \n2. Ustaw minuty \n3. Ustaw godziny\n4. Wyswietl czas\n5. Opusc program\n";
-		std::cin >> option;
+	//while (menu) {
+	//	char option;
+	//	std::cout << "1. Ustaw sekundy \n2. Ustaw minuty \n3. Ustaw godziny\n4. Wyswietl czas\n5. Opusc program\n";
+	//	std::cin >> option;
 
-		switch (option) {
-		case '1': 
-			system("CLS");
-			std::cout << "Podaj liczbe sekund: ";
-			std::cin >> number;
-			clock.setSeconds(number);
-			break;
-		case '2':
-			system("CLS");
-			std::cout << "Podaj liczbe minut: ";
-			std::cin >> number;
-			clock.setMinutes(number);
-			break;
-		case '3':
-			system("CLS");
-			std::cout << "Podaj liczbe godzin: ";
-			std::cin >> number;
-			clock.setHours(number);
-			break;
-		case '4':
-			submenu = true;
-			system("CLS");
-			clock.printActualTime();
-			
-			break;
-		case '5':
-			system("CLS");
-			menu = false;
-			break;
-		default:
-			std::cout << "Zla opcja";
-			break;
-		}
-	}
+	//	switch (option) {
+	//	case '1': 
+	//		system("CLS");
+	//		std::cout << "Podaj liczbe sekund: ";
+	//		std::cin >> number;
+	//		clock.setSeconds(number);
+	//		break;
+	//	case '2':
+	//		system("CLS");
+	//		std::cout << "Podaj liczbe minut: ";
+	//		std::cin >> number;
+	//		clock.setMinutes(number);
+	//		break;
+	//	case '3':
+	//		system("CLS");
+	//		std::cout << "Podaj liczbe godzin: ";
+	//		std::cin >> number;
+	//		clock.setHours(number);
+	//		break;
+	//	case '4':
+	//		submenu = true;
+	//		system("CLS");
+	//		clock.printActualTime();
+	//		
+	//		break;
+	//	case '5':
+	//		system("CLS");
+	//		menu = false;
+	//		break;
+	//	default:
+	//		std::cout << "Zla opcja";
+	//		break;
+	//	}
+	//}
 
 	clock.printActualTime();
 }
